ca940cf46ebc6f59ef07f1786e6ab8fa8d015d1c
Administrator <2466921206@qq.com>
2020-03-27 18:14:42 +0800

    第二次提交

--- app/src/main/java/com/guangzhida/xiaomai/view/chat/EaseChatPrimaryMenu.kt
+++ app/src/main/java/com/guangzhida/xiaomai/view/chat/EaseChatPrimaryMenu.kt
@@ -1,10 +1,14 @@
 package com.guangzhida.xiaomai.view.chat
 
 import android.content.Context
+import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import com.guangzhida.xiaomai.R
+import com.guangzhida.xiaomai.ext.addTextChangedListener
+import kotlinx.android.synthetic.main.layout_chat_primary_menu.view.*
 
 /**
  * 输入框 语音和发送
@@ -20,6 +24,48 @@ class EaseChatPrimaryMenu @JvmOverloads constructor(
 
     init {
         LayoutInflater.from(context).inflate(R.layout.layout_chat_primary_menu, this)
+        //点击声音按钮
+        btn_set_mode_voice.setOnClickListener {
+
+        }
+        //点击键盘按钮
+        btn_set_mode_keyboard.setOnClickListener {
+
+        }
+        //按住说话
+        btn_press_to_speak.setOnTouchListener { v, event ->
+
+            false
+        }
+        //点击表情按钮 - 正常状态
+        iv_face_normal.setOnClickListener {
+
+        }
+        //点击表情按钮 - 选中状态
+        iv_face_checked.setOnClickListener {
+
+        }
+        //点击+号
+        btn_more.setOnClickListener {
+
+        }
+        //发送按钮
+        btn_send.setOnClickListener {
+
+        }
+        //监听输入框
+        et_sendmessage.addTextChangedListener {
+            afterTextChanged {
+                val text = et_sendmessage.text.toString().trim()
+                if (text.isEmpty()) {
+//                    buttonMore.setVisibility(View.GONE)
+//                    buttonSend.setVisibility(View.VISIBLE)
+                } else {
+//                    buttonMore.setVisibility(View.VISIBLE)
+//                    buttonSend.setVisibility(View.GONE)
+                }
+            }
+        }
 
     }
 
