6a3fc3e21caace629b96d601b7990bb18d57c136
Administrator <2466921206@qq.com>
2020-06-05 16:20:14 +0800

    测试修改提交分支

--- app/src/main/java/com/guangzhida/xiaomai/ui/chat/viewmodel/ChatServiceMessageViewModel.kt
+++ app/src/main/java/com/guangzhida/xiaomai/ui/chat/viewmodel/ChatServiceMessageViewModel.kt
@@ -329,7 +329,7 @@ open class ChatServiceMessageViewModel : BaseViewModel() {
                     conversationEntity?.avatarUrl = mServiceModel?.headUrl ?: ""
                     LogUtils.i("conversationEntity update=$conversationEntity")
                     mConversationDao?.update(conversationEntity)
-                    //....
+
                 }
             }
         }
